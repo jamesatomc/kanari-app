7362fd60a90feee603a23bf2492e01d7bf797039
jamesatomc <jiraphonwansai2002@gmail.com>
2024-12-09 11:50:40 +0700

    refactor: Remove unnecessary div wrapper around HeroSection in Home component

--- dapp/src/app/page.tsx
+++ dapp/src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-orange-950 via-gray-900 to-black">
       <WalletProvider>
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      
           <HeroSection />
-        </div>
+   
       </WalletProvider>
     </main>
   );
